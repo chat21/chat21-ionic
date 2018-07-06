6061e0e0cb40098465a1d3f7562dfc707ac4b831
Stefano De Pascalis <stefanodp91@gmail.com>
2018-07-06 10:30:43 +0200

    deploy dev prod

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 //TIMES
 // BUILD
-export const CURR_VER_DEV = '30';
-export const CURR_VER_PROD = '0.12';
+export const CURR_VER_DEV = '31';
+export const CURR_VER_PROD = '0.19';
 
 //export const SERVICE_WORKER_DEV = '/chat/';
 //export const SERVICE_WORKER_PROD = './chat/';
