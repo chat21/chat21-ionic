c854f54f8814c2d15a3c39b765138cc0d05722e8
Dario De Pascalis <dariodepa75@gmail.com>
2019-02-05 16:18:44 +0100

    deploy_dev

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -40,14 +40,18 @@ fi
 # echo 'build: ---->'$NEW_BUILD
 
 # sed -i -e "s/$start$ver.$build/$start$NEW_VER.$NEW_BUILD/g" src/utils/constants.ts
-# ionic cordova platform add browser --save
-# ionic cordova build browser
+sed -i -e "s/$URL_VER/g" src/utils/constants.ts
+ionic cordova platform add browser --save
+ionic cordova build browser
 
-# cp -p src/firebase-messaging-sw.js platforms/browser/www/
-# cp -p src/manifest.json platforms/browser/www/
-# cd platforms/browser/www
+cp -p src/firebase-messaging-sw.js platforms/browser/www/
+cp -p src/manifest.json platforms/browser/www/
+cd platforms/browser/www
 # aws s3 sync . s3://tiledesk-dashboard/chat/dev/$NEW_VER/$NEW_BUILD/
-# cd ../../../
+aws s3 sync . s3://tiledesk-dashboard/chat/dev/$URL_VER/
+cd ../../../
 
+echo new version deployed on s3://tiledesk-dashboard/chat/dev/$URL_VER/
+echo available on https://s3.eu-west-1.amazonaws.com/tiledesk-dashboard/chat/dev/$URL_VER/index.html
 # echo new version deployed on s3://tiledesk-dashboard/chat/dev/$NEW_VER/$NEW_BUILD/
 # echo available on https://s3.eu-west-1.amazonaws.com/tiledesk-dashboard/chat/dev/$NEW_VER/$NEW_BUILD/index.html
