95567eff97643dd120445342ef1345db527bf836
Dario De Pascalis <dariodepa75@gmail.com>
2019-03-14 15:35:16 +0100

    reset master

--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { _DetailPage } from './_DetailPage';
-export { _MasterPage } from './_MasterPage';
-export { DettaglioConversazionePage } from './dettaglio-conversazione/dettaglio-conversazione';
-export { ListaConversazioniPage } from './lista-conversazioni/lista-conversazioni';
-export { PlaceholderPage } from './placeholder/placeholder';
