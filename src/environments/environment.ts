8e712a186da91ebfe67410a26342fd11c7738ee4
Andrea Leo <andrealeo83@users.noreply.github.com>
2019-08-27 14:43:03 +0200

    changed env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
-import { firebaseConfig } from '../environments/firebase-config'; // please comment on this line when changing the values ​​of firebase {}
+// import { firebaseConfig } from '../environments/firebase-config'; // please comment on this line when changing the values ​​of firebase {}
 
 export const environment = {
     production: false,
     firebaseConfig : {
-        apiKey: firebaseConfig.apiKey,
-        authDomain: firebaseConfig.authDomain,
-        databaseURL: firebaseConfig.databaseURL,
-        projectId: firebaseConfig.projectId,
-        storageBucket: firebaseConfig.storageBucket,
-        messagingSenderId: firebaseConfig.messagingSenderId
+        apiKey: 'CHANGEIT',
+        authDomain: 'CHANGEIT',
+        databaseURL: 'CHANGEIT',
+        projectId: 'CHANGEIT',
+        storageBucket: 'CHANGEIT',
+        messagingSenderId: 'CHANGEIT'
     }
   }
