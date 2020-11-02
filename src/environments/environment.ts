0a402f12af7ac04e6cde0bbe78503f8f3ea6827c
Andrea Sponziello <andrea.sponziello@gmail.com>
2020-11-02 12:02:06 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,23 @@
 export const environment = {
-    tenant: 'CHANGEIT',
-    supportMode: true,
-    CHAT_SEND_BY_EMAIL_LINK: 'CHANGEIT',
-    FIREBASESTORAGE_BASE_URL_IMAGE: 'CHANGEIT',
-    DASHBOARD_URL: 'CHANGEIT',
-    SERVER_BASE_URL: 'CHANGEIT',
-    production: false,
-    remoteConfig: true,
-    remoteConfigUrl: './chat-config.json',
-    remoteContactsUrl: '',
-    firebaseConfig: {
-        apiKey: 'CHANGEIT',
-        authDomain: 'CHANGEIT',
-        databaseURL: 'CHANGEIT',
-        projectId: 'CHANGEIT',
-        storageBucket: 'CHANGEIT',
-        messagingSenderId: 'CHANGEIT',
-        appId: 'CHANGEIT',
-        chat21ApiUrl: 'CHANGEIT'
-    }
-};
+tenant: 'tilechat',
+supportMode: true,
+CHAT_SEND_BY_EMAIL_LINK: "mailto:",
+DASHBOARD_URL: "https://console.tiledesk.com/v2/dashboard/",
+FIREBASESTORAGE_BASE_URL_IMAGE: "https://firebasestorage.googleapis.com/v0/b/",
+SERVER_BASE_URL: "https://api.tiledesk.com/v2/",
+production: true,
+remoteConfig: false,
+remoteConfigUrl: './chat-config.json',
+remoteContactsUrl: 'https://api.tiledesk.com/v2/chat21/contacts',
+firebaseConfig: {
+    apiKey: "AIzaSyDKfdKrlD7AYcbQ-U-xxgV-b3FUQ4xt7NM",
+    authDomain: "tiledesk-prod-v2.firebaseapp.com",
+    databaseURL: "https://tiledesk-prod-v2.firebaseio.com",
+    projectId: "tiledesk-prod-v2",
+    storageBucket: "tiledesk-prod-v2.appspot.com",
+    messagingSenderId: "92907897826",
+    appId: "1:92907897826:web:f255664014a7cc14ee2fbb",
+    chat21ApiUrl: "https://us-central1-tiledesk-prod-v2.cloudfunctions.net"
+},
+authPersistence: 'LOCAL'
+}
