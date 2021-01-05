3bf0998da95785ca8171317c54a85d0d5fb3250e
andrea leo <andrea.leo83@gmail.com>
2021-01-05 13:17:22 +0100

    build prod sh

---
+++ build_prod.sh
@@ -0,0 +1,10 @@
+npm version patch
+version=`node -e 'console.log(require("./package.json").version)'`
+echo "version $version"
+
+if [ "$version" != "" ]; then
+    git tag -a "$version" -m "`git log -1 --format=%s`"
+    echo "Created a new tag, $version"
+    git push --tags
+    npm publish
+fi
