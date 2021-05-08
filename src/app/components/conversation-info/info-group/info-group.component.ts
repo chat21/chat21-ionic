7e870bbe7c65010a513c50c674abf4514dc6fb2b
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-05-08 20:20:38 +0200

    Adds the display of the members belonging to the group (avatar, name and online / offline status)

--- src/app/components/conversation-info/info-group/info-group.component.ts
+++ src/app/components/conversation-info/info-group/info-group.component.ts
@@ -1,32 +1,199 @@
-import { Component, OnInit, AfterViewInit , Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, OnChanges, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/chat21-core/providers/abstract/auth.service';
+import { ImageRepoService } from 'src/chat21-core/providers/abstract/image-repo.service';
+import { PresenceService } from 'src/chat21-core/providers/abstract/presence.service';
+import { ContactsService } from 'src/app/services/contacts/contacts.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+
+import { filter } from 'rxjs/operators';
 // import {avatarPlaceholder, getColorBck} from 'src/chat21-core/utils/utils-user';
 @Component({
   selector: 'app-info-group',
   templateUrl: './info-group.component.html',
   styleUrls: ['./info-group.component.scss'],
 })
-export class InfoGroupComponent implements OnInit, AfterViewInit{
+export class InfoGroupComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
+  // objectKeys = Object.keys;
 
-  @Input() groupDetail: any; 
+  @Input() groupDetail: any;
+  member_is_online;
+  member_array: any
+  private unsubscribe$: Subject<any> = new Subject<any>();
+  constructor(
+    public imageRepoService: ImageRepoService,
+    public presenceService: PresenceService,
+    public authService: AuthService,
+    public contactsService: ContactsService
+  ) {
 
-  constructor() {
+    console.log('InfoGroupComponent HELLO !!!')
+  }
 
-    console.log('InfoGroupComponent HELLO !!!' ) 
-   }
 
-  ngOnInit() { 
-    
-    console.log('InfoGroupComponent groupDetail', this.groupDetail);
+  ngOnChanges() {
+    console.log('InfoGroupComponent group detail ngOnChanges', this.groupDetail);
+
+    if (this.groupDetail) {
+      if (this.groupDetail.uid.startsWith('group-')) {
+        const tiledeskToken = this.authService.getTiledeskToken();
+
+        this.member_array = []
+        const members_isonline_array = []
+
+        for (const [key, value] of Object.entries(this.groupDetail.membersinfo)) {
+          console.log('CONVERSATION-DETAIL group detail Key:', key, ' -Value: ', value);
+
+
+          this.presenceService.userIsOnline(key)
+            .pipe(filter((isOnline) => isOnline !== null))
+            .subscribe((isOnline: any) => {
+              console.log('InfoGroupComponent group detail BSIsOnline isOnline', isOnline)
+              // this.member_is_online = isOnline;
+
+              // test 
+              this.groupDetail.membersinfo[key]['isSignin'] = isOnline.isOnline
+
+              if (this.member_array.length > 0) {
+                console.log('InfoGroupComponent group detail BSIsOnline HERE YES')
+                // this.member_array['userOnline'] = isOnline.isOnline
+                this.member_array.find(x => x.userid == isOnline.uid)['userOnline'] = isOnline.isOnline
+
+              }
+
+              console.log('InfoGroupComponent group detail BSIsOnline  this.groupDetail 2', this.groupDetail)
+
+              console.log('InfoGroupComponent group detail BSIsOnline isOnline member_array', this.member_array)
+              // this.member_array['userOnline'] = this.groupDetail.membersinfo[key]['isSignin']
+              // if (this.member_array.length > 0) {
+              //   // if (isOnline !== null) {
+              // this.member_array.find(x => x.userid == isOnline.uid)['userOnline'] = isOnline.isOnline
+              // this.member_array.find(x => x.userid == isOnline.uid)['userOnlineUID'] = isOnline.uid;
+              //   console.log('InfoGroupComponent group detail BSIsOnline isOnline member_array 2', this.member_array)
+              //   console.log('InfoGroupComponent group detail after assignment ', this.groupDetail)
+              // }
+              // console.log('InfoGroupComponent group detail BSIsOnline isOnline', this.member_is_online)
+
+
+            })
+
+
+            this.contactsService.loadContactDetail(tiledeskToken, key)
+            .subscribe(user => {
+              console.log('InfoGroupComponent group detail loadContactDetail RES', user);
+              // console.log('InfoGroupComponent group detail this.presenceService.BSIsOnline.value()', this.presenceService.BSIsOnline.getValue);
+
+              user.imageurl = this.imageRepoService.getImagePhotoUrl(key)
+              // this.member_array.push({ userid: user.uid, avatar: user.avatar, color: user.color, email: user.email, fullname: user.fullname, imageurl: user.imageurl, userOnline: isOnline })
+              this.member_array.push({ userid: user.uid, avatar: user.avatar, color: user.color, email: user.email, fullname: user.fullname, imageurl: user.imageurl, userOnline: this.groupDetail.membersinfo[user.uid]['isSignin'] })
+
+            }, (error) => {
+              console.log('InfoGroupComponent group detail loadContactDetail - ERROR  ', error);
+            }, () => {
+              console.log('InfoGroupComponent group detail loadContactDetail * COMPLETE *');
+
+            });
+
+
+          // this.contactsService.loadContactDetail(tiledeskToken, key)
+          //   .subscribe(user => {
+          //     console.log('InfoGroupComponent group detail loadContactDetail RES', user);
+          //     // console.log('InfoGroupComponent group detail this.presenceService.BSIsOnline.value()', this.presenceService.BSIsOnline.getValue);
+
+          //     user.imageurl = this.imageRepoService.getImagePhotoUrl(key)
+          //     this.member_array.push({ userid: user.uid, avatar: user.avatar, color: user.color, email: user.email, fullname: user.fullname, imageurl: user.imageurl, userOnline: false })
+          //     // this.member_array.push({ userid: user.uid, avatar: user.avatar, color: user.color, email: user.email, fullname: user.fullname, imageurl: user.imageurl, userOnline: this.groupDetail.membersinfo[user.uid]['isSignin'] })
+
+          //   }, (error) => {
+          //     console.log('InfoGroupComponent group detail loadContactDetail - ERROR  ', error);
+          //   }, () => {
+          //     console.log('InfoGroupComponent group detail loadContactDetail * COMPLETE *');
+
+          //   });
+
+        }
+
+
+        this.groupDetail['member_array'] = this.member_array
+        console.log('InfoGroupComponent group detail after at the end', this.member_array);
+
+        // // 2nd for loop fo presence
+        // for (const [key, value] of Object.entries(this.groupDetail.membersinfo)) {
+        //   console.log('CONVERSATION-DETAIL group detail Key:', key, ' -Value: ', value);
+
+
+        //   this.presenceService.userIsOnline(key)
+        //     .pipe(filter((isOnline) => isOnline !== null))
+        //     .subscribe((isOnline: any) => {
+        //       console.log('InfoGroupComponent group detail BSIsOnline isOnline', isOnline)
+        //       // this.member_is_online = isOnline;
+
+        //       // test 
+        //       // this.groupDetail.membersinfo[key]['isSignin'] = isOnline.isOnline
+
+        //       console.log('InfoGroupComponent group detail BSIsOnline  this.groupDetail 2', this.groupDetail)
+
+        //       console.log('InfoGroupComponent group detail BSIsOnline isOnline member_array', this.member_array)
+        //       // this.member_array['userOnline'] = this.groupDetail.membersinfo[key]['isSignin']
+        //       if (this.member_array.length > 0) {
+        //       //   // if (isOnline !== null) {
+        //       this.member_array.find(x => x.userid == isOnline.uid)['userOnline'] = isOnline.isOnline
+        //       this.member_array.find(x => x.userid == isOnline.uid)['userOnlineUID'] = isOnline.uid;
+        //       //   console.log('InfoGroupComponent group detail BSIsOnline isOnline member_array 2', this.member_array)
+        //       //   console.log('InfoGroupComponent group detail after assignment ', this.groupDetail)
+        //       }
+        //       // console.log('InfoGroupComponent group detail BSIsOnline isOnline', this.member_is_online)
+        //     })
+
+
+
+
+
+        //   // this.contactsService.loadContactDetail(tiledeskToken, key)
+        //   //   .subscribe(user => {
+        //   //     console.log('InfoGroupComponent group detail loadContactDetail RES', user);
+        //   //     // console.log('InfoGroupComponent group detail this.presenceService.BSIsOnline.value()', this.presenceService.BSIsOnline.getValue);
+
+        //   //     user.imageurl = this.imageRepoService.getImagePhotoUrl(key)
+        //   //     // this.member_array.push({ userid: user.uid, avatar: user.avatar, color: user.color, email: user.email, fullname: user.fullname, imageurl: user.imageurl, userOnline: false })
+        //   //     this.member_array.push({ userid: user.uid, avatar: user.avatar, color: user.color, email: user.email, fullname: user.fullname, imageurl: user.imageurl, userOnline: this.groupDetail.membersinfo[user.uid]['isSignin'] })
+
+        //   //   }, (error) => {
+        //   //     console.log('InfoGroupComponent group detail loadContactDetail - ERROR  ', error);
+        //   //   }, () => {
+        //   //     console.log('InfoGroupComponent group detail loadContactDetail * COMPLETE *');
+
+        //   //   });
+
+        // }
+
+        // console.log('InfoGroupComponent group detail after at the end 2nd loop', this.member_array);
+
+      }
+    }
+  }
+
+  ngOnInit() {
+
 
     // this.groupDetail.avatar = avatarPlaceholder(this.groupDetail.name);
     // this.groupDetail.color = getColorBck(this.groupDetail.name);
-      
-}
+
+  }
 
   ngAfterViewInit() {
     console.log('InfoGroupComponent - ngAfterViewInit');
-   
+
     // console.log('InfoGroupComponent conversationWith', this.conversationWith);
+  }
+
+  ngOnDestroy() {
+    // console.log('ngOnDestroy ConversationDetailPage: ');
+
+    console.log('InfoGroupComponent group detail ngOnDestroy');
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
--- src/chat21-core/providers/firebase/firebase-presence.service.ts
+++ src/chat21-core/providers/firebase/firebase-presence.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 // firebase
 import * as firebase from 'firebase/app';
@@ -20,8 +20,10 @@ import { CustomLogger } from '../logger/customLogger';
 export class FirebasePresenceService extends PresenceService {
 
   // BehaviorSubject
+  // BSIsOnline: BehaviorSubject<any>;
   BSIsOnline: BehaviorSubject<any>;
   BSLastOnline: BehaviorSubject<any>;
+
 
   // public params
   public tenant: string;
@@ -32,7 +34,7 @@ export class FirebasePresenceService extends PresenceService {
   private lastOnlineConnectionsRef: any;
   private keyConnectionRef: any;
   private logger: CustomLogger = new CustomLogger(true)
-
+  online_member = []
   constructor() {
     super();
   }
@@ -48,21 +50,52 @@ export class FirebasePresenceService extends PresenceService {
    * userIsOnline
    * @param userid
    */
-  public userIsOnline(userid: string) {
+  // public userIsOnline(userid: string) {
+  //   this.logger.printDebug('userIsOnline', userid);
+  //   console.log('CONVERSATION-DETAIL group detail userIsOnline', userid);
+  //   const that = this;
+  //   const urlNodeConnections = this.urlNodePresence + userid + '/connections';
+  //   this.logger.printDebug('userIsOnline: ', urlNodeConnections);
+  //   const connectionsRef = firebase.database().ref().child(urlNodeConnections);
+  //   connectionsRef.on('value', (child) => {
+  //     this.logger.printDebug('is-online-' + userid);
+  //     if (child.val()) {
+  //       console.log('CONVERSATION-DETAIL group detail userIsOnline id user', userid, '- child.val: ', child.val());
+  //       this.BSIsOnline.next({uid: userid, isOnline: true});
+  //       console.log('CONVERSATION-DETAIL group detail userIsOnline 1', userid);
+  //     } else {
+  //       this.BSIsOnline.next({uid: userid, isOnline: false});
+  //       console.log('CONVERSATION-DETAIL group detail userIsOnline 2', userid);
+  //     }
+  //   });
+  // }
+
+  public userIsOnline(userid: string): Observable<any> {
+    let local_BSIsOnline = new BehaviorSubject<any>(null);
     this.logger.printDebug('userIsOnline', userid);
+    console.log('CONVERSATION-DETAIL group detail userIsOnline', userid);
     const that = this;
     const urlNodeConnections = this.urlNodePresence + userid + '/connections';
     this.logger.printDebug('userIsOnline: ', urlNodeConnections);
     const connectionsRef = firebase.database().ref().child(urlNodeConnections);
+    connectionsRef.off()
     connectionsRef.on('value', (child) => {
       this.logger.printDebug('is-online-' + userid);
       if (child.val()) {
-        this.BSIsOnline.next({uid: userid, isOnline: true});
+        console.log('CONVERSATION-DETAIL group detail userIsOnline id user', userid, '- child.val: ', child.val());
+        this.BSIsOnline.next({ uid: userid, isOnline: true });
+        local_BSIsOnline.next({ uid: userid, isOnline: true });
+        console.log('CONVERSATION-DETAIL group detail userIsOnline 1', userid);
       } else {
-        this.BSIsOnline.next({uid: userid, isOnline: false});
+        this.BSIsOnline.next({ uid: userid, isOnline: false });
+        local_BSIsOnline.next({ uid: userid, isOnline: false });
+        console.log('CONVERSATION-DETAIL group detail userIsOnline 2', userid);
       }
     });
+
+    return local_BSIsOnline
   }
+
 
   /**
    * lastOnlineForUser
@@ -74,9 +107,9 @@ export class FirebasePresenceService extends PresenceService {
     const lastOnlineRef = this.referenceLastOnlineForUser(userid);
     lastOnlineRef.on('value', (child) => {
       if (child.val()) {
-        this.BSLastOnline.next({uid: userid, lastOnline: child.val()});
+        this.BSLastOnline.next({ uid: userid, lastOnline: child.val() });
       } else {
-        this.BSLastOnline.next({uid: userid, lastOnline: null});
+        this.BSLastOnline.next({ uid: userid, lastOnline: null });
       }
     });
   }
@@ -94,7 +127,7 @@ export class FirebasePresenceService extends PresenceService {
    *  https://firebase.google.com/docs/database/web/offline-capabilities?hl=it
    * @param userid
    */
-  public setPresence(userid: string): void  {
+  public setPresence(userid: string): void {
     //console.log(' setPresence: ', userid);
     this.onlineConnectionsRef = this.referenceOnlineForUser(userid);
     this.lastOnlineConnectionsRef = this.referenceLastOnlineForUser(userid);
