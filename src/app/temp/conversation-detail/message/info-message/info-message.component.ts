c57179cf4cca26fdd50f8d5d6c6c877aff9d0c3f
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-06-03 12:38:37 +0200

    Fixes the bug: if a snippet of code is pasted and sent it is not displayed correctly info message + adds left and right margin

--- src/app/temp/conversation-detail/message/info-message/info-message.component.ts
+++ src/app/temp/conversation-detail/message/info-message/info-message.component.ts
@@ -1,18 +1,34 @@
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { MessageModel } from 'src/chat21-core/models/message';
+import { htmlEntities } from 'src/chat21-core/utils/utils';
 
 @Component({
   selector: 'tiledeskwidget-info-message',
   templateUrl: './info-message.component.html',
   styleUrls: ['./info-message.component.scss']
 })
-export class InfoMessageComponent implements OnInit {
+export class InfoMessageComponent implements OnInit, OnChanges {
 
   @Input() message: MessageModel
+
+ public message_text: string 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+
+    console.log('InfoMessageComponent message ', this.message)
+    
+         // Fixes the bug: if a snippet of code is pasted and sent it is not displayed correctly info message
+         if (this.message && this.message.text) {
+          var regex = /<br\s*[\/]?>/gi;
+          this.message.text = this.message.text.replace(regex, "\n")
+          this.message_text = this.message.text
+          console.log('InfoMessageComponent message .text  ', this.message.text )
+        }
   }
 
 }
