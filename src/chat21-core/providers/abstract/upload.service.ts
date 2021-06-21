cc04d635458696ba11ad01f2cf4517652be3a1da
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-06-21 17:57:34 +0200

    Commented unused code

--- src/chat21-core/providers/abstract/upload.service.ts
+++ src/chat21-core/providers/abstract/upload.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
-//import { environment } from 'src/environments/environment';
+
 
 // models
 import { UploadModel } from '../../models/upload';
