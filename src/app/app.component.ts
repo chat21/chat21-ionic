e205021963adf5ab66a6e448266f214d1f97ba9b
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-07-07 12:19:50 +0200

    Commented the method createExternalSidebar()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -354,8 +354,13 @@ export class AppComponent implements OnInit {
 
       this.router.navigateByUrl(pageUrl);
 
-      // const DASHBOARD_URL = this.appConfigProvider.getConfig().dashboardUrl;
-      // createExternalSidebar(this.renderer, dashboardUrl);
+    
+      // const DASHBOARD_URL = this.appConfigProvider.getConfig().DASHBOARD_URL;
+      // // createExternalSidebar(this.renderer, DASHBOARD_URL);
+
+      // // FOR REALTIME TESTING
+      // createExternalSidebar(this.renderer, 'http://localhost:4203');
+     
     }
   }
 
