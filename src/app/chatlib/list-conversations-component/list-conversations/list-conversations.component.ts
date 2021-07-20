b5d5ac56994614306765ac520b0db2c0308db696
Gabriele Panico <gabriele.panico95@gmail.com>
2021-07-20 10:43:03 +0200

    bug fixed on conversation changed for mqtt

--- src/app/chatlib/list-conversations-component/list-conversations/list-conversations.component.ts
+++ src/app/chatlib/list-conversations-component/list-conversations/list-conversations.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, IterableDiffers, KeyValueDiffer, KeyValueDiffers, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, IterableChangeRecord, IterableDiffers, KeyValueDiffer, KeyValueDiffers, OnInit, Output, SimpleChanges } from '@angular/core';
 import { ConversationModel } from '../../../../chat21-core/models/conversation';
 import { ImageRepoService } from '../../../../chat21-core/providers/abstract/image-repo.service';
 
@@ -10,7 +10,7 @@ import { ImageRepoService } from '../../../../chat21-core/providers/abstract/ima
 export class ListConversationsComponent implements OnInit {
 
   // ========= begin:: Input/Output values ============//
-  @Input() listConversations: Array<ConversationModel>;
+  @Input() listConversations: ConversationModel[];
   @Input() limit?: number
   @Input() stylesMap: Map<string, string>;
   @Input() translationMap: Map< string, string>;
@@ -23,8 +23,8 @@ export class ListConversationsComponent implements OnInit {
   uidConvSelected: string;
   constructor(public iterableDiffers: IterableDiffers,
               public imageRepoService: ImageRepoService) {
-          this.iterableDifferListConv = this.iterableDiffers.find([]).create(null);
-              }
+          this.iterableDifferListConv = this.iterableDiffers.find([]).create(null);   
+        }
 
   ngOnInit() {
     console.log(' ngOnInit::::list-conversations ', this.listConversations);
@@ -48,15 +48,33 @@ export class ListConversationsComponent implements OnInit {
   ngDoCheck() {
     let changesListConversation = this.iterableDifferListConv.diff(this.listConversations);
     if(changesListConversation){
-      changesListConversation.forEachAddedItem(element => {
-        //console.log('1111 added ', element)
-        let conv = element.item
-        this.onImageLoaded.emit(conv)
-        this.onConversationLoaded.emit(conv)
-      });
-      changesListConversation.forEachRemovedItem(element => {
-        //console.log('1111 removed ', element)
-      });
+      // changesListConversation.forEachAddedItem(element => {
+      //   console.log('itemmmm 1111 added ', element)
+      //   let conv = element.item
+      //   this.onImageLoaded.emit(conv)
+      //   this.onConversationLoaded.emit(conv)
+      // });
+      // changesListConversation.forEachRemovedItem(element => {
+      //   console.log('itemmmm 1111 removed ', element)
+      // });
+      // changesListConversation.forEachOperation((element: IterableChangeRecord<ConversationModel>, adjustedPreviousIndex: number, currentIndex: number) => {
+      //   // if (item.previousIndex == null) {
+      //   //   console.log('itemmmm 1111', item, adjustedPreviousIndex)
+      //   // } else if (currentIndex == null) {
+      //   //   console.log('itemmmm 2222', item, adjustedPreviousIndex)
+      //   // } else {
+      //   //   console.log('itemmmm 3333', item, adjustedPreviousIndex)
+      //   // }
+      //   if(element.currentIndex == null || element.previousIndex == null){
+      //     console.log('itemmmm 44444', element, adjustedPreviousIndex, currentIndex)
+      //     let conv = element.item
+      //     this.onImageLoaded.emit(conv)
+      //     this.onConversationLoaded.emit(conv)
+      //   }
+      // });
+      // changesListConversation.forEachItem((item: IterableChangeRecord<ConversationModel>)=> {
+      //   console.log('itemmmm forEachItem', item)
+      // });
       //Detect changes in array when item added or removed
       // let empArrayChanges = this.objDiffers.diff(this.listConversations);
       // if (empArrayChanges) {
@@ -69,6 +87,8 @@ export class ListConversationsComponent implements OnInit {
       //     console.log('1111 Removed ' + record.previousValue);
       //   });
       // }
+
+
     }
 
     //Detect changes in object inside array
