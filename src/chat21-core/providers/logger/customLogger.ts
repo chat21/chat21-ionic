a1f7db39f8d6024c29d1dd6b4e684e965760c357
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-07-30 17:05:07 +0200

    Adds the 'log method'

--- src/chat21-core/providers/logger/customLogger.ts
+++ src/chat21-core/providers/logger/customLogger.ts
@@ -1,11 +1,13 @@
 import { LogLevel } from './../../utils/constants';
 import { Inject, Injectable } from '@angular/core';
-import { NgxLinkifyjsModule } from 'ngx-linkifyjs';
 import { NGXLogger, NGXLoggerMonitor, NGXLogInterface } from 'ngx-logger';
 import { LoggerService } from './../abstract/logger.service';
 @Injectable()
 export class CustomLogger implements LoggerService {
-
+    // Error = 0,
+    // Warn = 1,
+    // Info = 2,
+    // Debug = 3
 
     //private variables
     // private logger: NGXLogger 
@@ -14,9 +16,25 @@ export class CustomLogger implements LoggerService {
     constructor(@Inject('isLogEnabled') private isLogEnabled: boolean, private logger: NGXLogger) {
     }
 
-    setLoggerConfig(isLogEnabled: boolean, logLevel: number){
+    setLoggerConfig(isLogEnabled: boolean, logLevel: number) {
         this.isLogEnabled = isLogEnabled;
         this.logLevel = logLevel;
+        console.log('LoggerService this.logLevel  ', this.logLevel)
+
+    }
+
+    error(...message: any[]) {
+        if (this.isLogEnabled && this.logLevel >= LogLevel.Error) {
+            this.logger.error(message)
+            // console.log(message)
+        }
+    }
+
+    warn(...message: any[]) {
+        if (this.isLogEnabled && this.logLevel >= LogLevel.Warn) {
+            this.logger.warn(message)
+            // console.log(message)
+        }
     }
 
     info(...message: any[]) {
@@ -25,22 +43,19 @@ export class CustomLogger implements LoggerService {
             // console.log(message)
         }
     }
+
     debug(...message: any[]) {
+        
         if (this.isLogEnabled && this.logLevel >= LogLevel.Debug) {
             this.logger.debug(message)
-            // console.log(message)
+            // console.debug(message)
         }
     }
-    warn(...message: any[]) {
-        if (this.isLogEnabled && this.logLevel >= LogLevel.Warn) {
-            this.logger.warn(message)
-            // console.log(message)
-        }
-    }
-    error(...message: any[]) {
-        if (this.isLogEnabled && this.logLevel >= LogLevel.Error) {
-            this.logger.error(message)
-            // console.log(message)
+
+    log(...message: any[]) {
+        if (this.isLogEnabled && this.logLevel >= LogLevel.Debug) {
+            // console.log(message);
+            this.logger.log(message)
         }
     }
 
