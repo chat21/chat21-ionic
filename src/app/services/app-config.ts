f79998443066e46dda0d5aedcd6687136ca151d7
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-08-25 15:56:47 +0200

    Commented log

--- src/app/services/app-config.ts
+++ src/app/services/app-config.ts
@@ -2,9 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
+// Logger
+// import { LoggerService } from 'src/chat21-core/providers/abstract/logger.service';
+// import { LoggerInstance } from 'src/chat21-core/providers/logger/loggerInstance';
+
 @Injectable({ providedIn: 'root' })
 export class AppConfigProvider {
   private appConfig: any;
+  // private logger: LoggerService = LoggerInstance.getInstance()
+
   constructor(public http: HttpClient) {
     this.appConfig = environment;
     console.log('AppConfigProvider constructor environment:: ', environment);
