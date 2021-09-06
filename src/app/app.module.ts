171743b945cc7db061375fd10be4718dc23a7993
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-09-06 17:15:17 +0200

    Adds serverLoggingUrl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -261,7 +261,8 @@ const appInitializerFn = (appConfig: AppConfigProvider, logger: NGXLogger) => {
       serverLogLevel: NgxLoggerLevel.ERROR,
       timestampFormat: 'HH:mm:ss.SSS',
       enableSourceMaps: false,
-      colorScheme: ['purple', 'yellow', 'gray', 'gray', 'red', 'red', 'red']
+      colorScheme: ['purple', 'yellow', 'gray', 'gray', 'red', 'red', 'red'],
+      serverLoggingUrl: 'http://tiledesk-server-pre.herokuapp.com/logs'
     }),
     ScrollbarThemeModule,
     SharedModule,
