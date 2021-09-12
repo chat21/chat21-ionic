ac610c9618ecbb7d1109ab08d6b9ea9730d5e6f5
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-09-12 18:41:21 +0200

    Translates the date "how long ago" the message was sent in the conversation list + Adds the date the message was sent to the list of archived conversations

--- src/app/chatlib/list-conversations-component/ion-list-conversations/ion-list-conversations.component.ts
+++ src/app/chatlib/list-conversations-component/ion-list-conversations/ion-list-conversations.component.ts
@@ -6,7 +6,8 @@ import { ListConversationsComponent } from '../list-conversations/list-conversat
 import { Platform } from '@ionic/angular';
 import { LoggerService } from 'src/chat21-core/providers/abstract/logger.service';
 import { LoggerInstance } from 'src/chat21-core/providers/logger/loggerInstance';
-
+import { TranslateService } from '@ngx-translate/core';
+import * as moment from 'moment';
 // import { LoggerService } from 'src/chat21-core/providers/abstract/logger.service';
 // import { LoggerInstance } from 'src/chat21-core/providers/logger/loggerInstance';
 
@@ -22,7 +23,9 @@ export class IonListConversationsComponent extends ListConversationsComponent im
 
   convertMessage = convertMessage;
   isApp: boolean = false;
-  public logger: LoggerService = LoggerInstance.getInstance()
+  public logger: LoggerService = LoggerInstance.getInstance();
+  public currentYear: any
+  public browserLang: string
   /**
    * 
    * @param iterableDiffers 
@@ -32,20 +35,35 @@ export class IonListConversationsComponent extends ListConversationsComponent im
   constructor(
     public iterableDiffers: IterableDiffers,
     public kvDiffers: KeyValueDiffers,
-    public platform: Platform
+    public platform: Platform,
+    private translate: TranslateService,
   ) {
     super(iterableDiffers, kvDiffers)
+    this.browserLang = this.translate.getBrowserLang();
+    if (this.browserLang) {
+      if (this.browserLang === 'it') {
+        // this.translate.use('it');
+        moment.locale('it')
+
+      } else {
+        // this.translate.use('en');
+        moment.locale('en')
+      }
+    }
+
+    this.currentYear = moment().format('YYYY');
+    this.logger.log('[ION-LIST-CONVS-COMP] - currentYear ', this.currentYear)
   }
 
   ngOnInit() {
     this.isApp = this.platform.is('ios') || this.platform.is('android')
-    this.logger.log('[ION-LIST-CONVS-COMP] - ngOnInit - IS-APP ',this.isApp )
+    this.logger.log('[ION-LIST-CONVS-COMP] - ngOnInit - IS-APP ', this.isApp)
     this.logger.log('[ION-LIST-CONVS-COMP] - ngOnInit - Platform', this.platform.platforms());
-   }
+  }
 
   closeConversation(conversation: ConversationModel) {
     var conversationId = conversation.uid;
-    this.logger.log('[ION-LIST-CONVS-COMP] - closeConversation - conversationId ',conversationId )
+    this.logger.log('[ION-LIST-CONVS-COMP] - closeConversation - conversationId ', conversationId)
     this.onCloseConversation.emit(conversation)
   }
 
