133718e11cea6793ebc45d6c592465077d9aa3c1
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-10-05 18:10:16 +0200

    Fixes the bug: if the image name is longer than the image width when uploading, it is displayed on the right side of the image

--- src/app/chatlib/conversation-detail/message/image/image.component.ts
+++ src/app/chatlib/conversation-detail/message/image/image.component.ts
@@ -11,17 +11,49 @@ export class ImageComponent implements OnInit {
   @Input() width: string;
   @Input() height: number;
   @Output() onImageRendered = new EventEmitter<boolean>();
+
   loading: boolean = true
+  tooltipMessage: string;
+
+  tooltipOptions = {
+    'show-delay': 0,
+    'tooltip-class': 'chat-tooltip',
+    'theme': 'light',
+    'shadow': false,
+    'hide-delay-mobile': 0,
+    'hideDelayAfterClick': 3000,
+    'hide-delay': 200
+  };
 
   constructor() { }
 
   ngOnInit() {
+    this.getBrowserLanguageAndTranslateTooltipMsg()
+  }
+  getBrowserLanguageAndTranslateTooltipMsg() {
+    var userLang = navigator.language;
+    console.log('Image COMP - userLang ', userLang)
+    if (userLang === 'en') {
+      this.tooltipMessage = "Click to download"
+    } else {
+      this.tooltipMessage = "Clicca per scaricare"
+    }
   }
 
-  onLoaded(event){
+  onLoaded(event) {
     this.loading = false
     this.onImageRendered.emit(true)
   }
 
-
+  downloadImage(url: string, fileName: string) {
+    // console.log('Image COMP - IMAGE URL ', url) 
+    // console.log('Image COMP - IMAGE FILENAME ', fileName) 
+    const a: any = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.style = 'display: none';
+    a.click();
+    a.remove();
+  }
 }
