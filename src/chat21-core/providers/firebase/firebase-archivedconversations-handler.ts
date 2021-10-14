2895d52c54a45025164cbdaf9b2862d45d4821fd
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-10-14 13:09:44 +0200

    Fixes the bug: by clicking on an archived chat the avatar displayed in the header does not correspond to the one displayed in the conversation list and in the right side panel "conversation info"

--- src/chat21-core/providers/firebase/firebase-archivedconversations-handler.ts
+++ src/chat21-core/providers/firebase/firebase-archivedconversations-handler.ts
@@ -391,6 +391,7 @@ export class FirebaseArchivedConversationsHandler extends ArchivedConversationsH
         }
         // Fixes the bug: if a snippet of code is pasted and sent it is not displayed correctly in the archived convesations list
         // conv.last_message_text = htmlEntities(conv.last_message_text)
+        conv.conversation_with = conversation_with;
         conv.conversation_with_fullname = conversation_with_fullname;
         conv.status = this.setStatusConversation(conv.sender, conv.uid);
         // conv.time_last_message = this.getTimeLastMessage(conv.timestamp); // evaluate if is used
