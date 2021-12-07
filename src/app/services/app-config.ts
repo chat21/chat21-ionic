3d1dad1835e30644dc725fac8a0b1bf91514685a
nicola74 <nicola.lanzilotto@frontiere21.it>
2021-12-07 19:05:48 +0100

    Modifies the "app-config" service by adding the ability to pass relative URLs to the websocket

--- src/app/services/app-config.ts
+++ src/app/services/app-config.ts
@@ -18,18 +18,88 @@ export class AppConfigProvider {
 
   /** */
   loadAppConfig() {
-    const that = this;
-    return this.http.get(this.appConfig.remoteConfigUrl).toPromise().then(data => {
-        that.appConfig = data;
+    // const that = this;
+    // return this.http.get(this.appConfig.remoteConfigUrl).toPromise().then(data => {
+    //     that.appConfig = data;
+    //   }).catch(err => {
+    //     console.log('error loadAppConfig' + err);
+    //   });
+
+    // ---- new
+
+
+    return this.http.get(this.appConfig.remoteConfigUrl)
+      .toPromise().then((data: any ) => {
+        console.log('AppConfigService loadAppConfig data: ', data);
+ 
+        const allconfig = data
+        // console.log('[APP-CONFIG-SERVICE] - loadAppConfig allconfig: ', allconfig);
+
+        if (allconfig.hasOwnProperty('wsUrlRel')) {
+
+          // console.log('[APP-CONFIG-SERVICE] - loadAppConfig allconfig !!!! exist wsUrlRel ->: ', allconfig.wsUrlRel);
+          var wsUrlRelIsEmpty = this.isEmpty(allconfig.wsUrlRel)
+          // console.log('[APP-CONFIG-SERVICE] - loadAppConfig allconfig !!!! exist wsUrlRel -> wsUrlRelIsEmpty ?', wsUrlRelIsEmpty);
+
+          if (wsUrlRelIsEmpty === false) {
+            // console.log('[APP-CONFIG-SERVICE]- loadAppConfig allconfig !!!! exist - SERVER_BASE_URL', data.apiUrl);
+
+            if (allconfig.apiUrl.indexOf("http://") !== -1) {
+
+              const ws_url = allconfig.apiUrl.replace("http://", "ws://").slice(0, -1) + allconfig.wsUrlRel;
+              // console.log('AppConfigService loadAppConfig allconfig !!!! exist - SERVER_BASE_URL protocol is HTTP - wsUrl', ws_url);
+
+              allconfig.wsUrl = ws_url
+
+            } else if (allconfig.apiUrl.indexOf("https://") !== -1) {
+
+              const ws_url = allconfig.apiUrl.replace("https://", "wss://").slice(0, -1) + allconfig.wsUrlRel;
+
+              allconfig.wsUrl = ws_url
+
+              // console.log('AppConfigService loadAppConfig allconfig !!!! exist - SERVER_BASE_URL protocol is HTTPS - wsUrl', ws_url);
+            } else {
+              console.log('AppConfigService loadAppConfig allconfig !!!! exist - SERVER_BASE_URL !!! IS RELATIVE - window.location ', window.location);
+
+              // console.log(window.location)
+
+              if (window.location.protocol === 'http:') {
+                allconfig.wsUrl = 'ws://' + window.location.hostname + '/ws/'
+
+              } else if (window.location.protocol === 'https:') {
+
+                allconfig.wsUrl = 'wss://' + window.location.hostname + '/ws/'
+              } else {
+
+                allconfig.wsUrl = 'ws://' + window.location.hostname + '/ws/'
+              }
+            }
+
+          } else {
+            console.log('[APP-CONFIG-SERVICE] loadAppConfig allconfig !!!! exist wsUrlRel but IS EMPTY');
+          }
+
+        } else {
+
+          console.log('[APP-CONFIG-SERVICE] loadAppConfig allconfig !!!! does not exist wsUrlRel');
+        }
+
+        this.appConfig = allconfig;
+      
+
       }).catch(err => {
-        // console.log('error loadAppConfig' + err);
+        console.error('error loadAppConfig' + err);
       });
+  }
+
+  isEmpty(wsUrlRel: string) {
+    return (wsUrlRel === undefined || wsUrlRel == null || wsUrlRel.length <= 0) ? true : false;
   }
 
   /** */
   getConfig() {
     return this.appConfig;
   }
-  
+
 
 }
