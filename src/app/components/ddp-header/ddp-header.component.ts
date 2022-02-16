dcb03afb92249a6945b5ecf96bb731c2e8f9d772
nicola74 <nicola.lanzilotto@frontiere21.it>
2022-02-16 19:57:30 +0100

    Moves the button to view archived conversations from the 'user information' modal in the header of the conversation list

--- src/app/components/ddp-header/ddp-header.component.ts
+++ src/app/components/ddp-header/ddp-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import { EventsService } from 'src/app/services/events-service';
 @Component({
   selector: 'app-ddp-header',
   templateUrl: './ddp-header.component.html',
@@ -10,9 +10,22 @@ export class DdpHeaderComponent implements OnInit {
   @Input() supportMode: boolean;
   @Output() openContactsDirectory = new EventEmitter();
   @Output() openProfileInfo = new EventEmitter();
+  IS_ON_MOBILE_DEVICE: boolean;
 
   constructor(
-  ) { }
+    public events: EventsService,
+  ) { 
+    this.isOnMobileDevice()
+  }
+
+  isOnMobileDevice() {
+    this.IS_ON_MOBILE_DEVICE = false;
+    if (/Android|iPhone/i.test(window.navigator.userAgent)) {
+      this.IS_ON_MOBILE_DEVICE = true;
+    }
+    console.log('[DDP-HEADER] IS_ON_MOBILE_DEVICE', this.IS_ON_MOBILE_DEVICE)
+    return this.IS_ON_MOBILE_DEVICE;
+  }
 
   ngOnInit() {
     // console.log('DDP HEADER SUPPORT MODE ', this.supportMode)
@@ -29,5 +42,9 @@ export class DdpHeaderComponent implements OnInit {
     this.openContactsDirectory.emit(e);
   }
   // END @Output() //
+
+  onClickArchivedConversation() {
+    this.events.publish('profileInfoButtonClick:changed', 'displayArchived');
+  }
 
 }
