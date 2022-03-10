e18ae08379c1052e0b6406f23a69703524dff6f4
nicola74 <nicola.lanzilotto@frontiere21.it>
2022-03-10 19:50:51 +0100

    Adds the ability to create a ticket

--- src/app/services/tiledesk/tiledesk.service.ts
+++ src/app/services/tiledesk/tiledesk.service.ts
@@ -110,6 +110,169 @@ export class TiledeskService {
     }))
   }
 
+
+  public getProjectUsersByProjectId(project_id: string, token: string) {
+    const url = this.apiUrl + project_id + '/project_users/';
+    console.log('[TILEDESK-SERVICE] - GET PROJECT-USER URL', url);
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+    return this.http
+    .get(url, httpOptions)
+    .pipe(map((res: any) => {
+      console.log('[TILEDESK-SERVICE] - GET PROJECT-USER RES ', res);
+      return res
+    }))
+  }
+
+  public getAllLeadsActiveWithLimit(project_id: string, token: string, limit: number) {
+    const url = this.apiUrl + project_id + '/leads?limit=' + limit + '&with_fullname=true';
+    console.log('[TILEDESK-SERVICE] - GET ALL ACTIVE LEADS (LIMIT 10000) -  URL', url);
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+    return this.http
+    .get(url, httpOptions)
+    .pipe(map((res: any) => {
+      console.log('[TILEDESK-SERVICE] - GET ALL ACTIVE LEADS (LIMIT 10000) ', res);
+      return res
+    }))
+  }
+
+
+  // ---------------------------------------------
+  // @ Create new project user to get new lead ID
+  // ---------------------------------------------
+  public createNewProjectUserToGetNewLeadID(project_id: string, token: string) {
+    const url = this.apiUrl + project_id + '/project_users/'
+    this.logger.log('[TILEDESK-SERVICE] - CREATE NEW PROJECT USER TO GET NEW LEAD ID url ', url);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+    const body = {};
+    return this.http
+      .post(url, body, httpOptions)
+      .pipe(map((res: any) => {
+        console.log('[TILEDESK-SERVICE] - CREATE NEW PROJECT USER TO GET NEW LEAD ID url ', res);
+        return res
+      }))
+  }
+
+  // ---------------------------------------------
+  // @ Create new lead 
+  // ---------------------------------------------
+  public createNewLead(leadid: string, fullname: string, leademail: string, project_id: string, token: string) {
+    const url = this.apiUrl + project_id + '/leads/'
+    console.log('[TILEDESK-SERVICE] - CREATE NEW LEAD url ', url);
+   
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+
+    const body = { 'lead_id': leadid, 'fullname': fullname, 'email': leademail };
+    console.log('[TILEDESK-SERVICE] - CREATE NEW LEAD ', body);
+
+    return this.http
+    .post(url, body, httpOptions)
+    .pipe(map((res: any) => {
+      console.log('[TILEDESK-SERVICE] - CREATE NEW LEAD RES ', res);
+      return res
+    }))
+  }
+
+  // -------------------------------------------------------------------------------------
+  // @ Get all bots of the project (with all=true the response return also the identity bot) 
+  // -------------------------------------------------------------------------------------
+  public getAllBotByProjectId(project_id: string, token: string) {
+   
+    const url = this.apiUrl + project_id + '/faq_kb?all=true'
+    console.log('[TILEDESK-SERVICE] - GET ALL BOTS BY PROJECT ID - URL', url);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+
+    return this.http
+    .get(url, httpOptions)
+    .pipe(map((res: any) => {
+      console.log('[TILEDESK-SERVICE] - GET ALL BOTS BY PROJECT ID - RES ', res);
+      return res
+    }))
+  }
+
+  // -------------------------------------------------------------------------------------
+  // @ Get all DEPTS of the project
+  // -------------------------------------------------------------------------------------
+  public getDeptsByProjectId(project_id: string, token: string) {
+   
+    const url = this.apiUrl + project_id + '/departments/allstatus';
+    console.log('[TILEDESK-SERVICE] - GET DEPTS (ALL STATUS) - URL', url);
+   
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+
+    return this.http
+    .get(url, httpOptions)
+    .pipe(map((res: any) => {
+      console.log('[TILEDESK-SERVICE] - GET DEPTS (ALL STATUS) - RES ', res);
+      return res
+    }))
+  }
+
+    // -----------------------------------------------------------------------------------------
+  // @ Create ticket
+  // -----------------------------------------------------------------------------------------
+  createInternalRequest(requester_id: string, request_id: string, subject: string, message: string, departmentid: string, participantid: string, ticketpriority: string, project_id: string, token: string) {
+    
+    const url = this.apiUrl + project_id + '/requests/' + request_id + '/messages'
+    this.logger.log('[WS-REQUESTS-SERV] - CREATE INTERNAL REQUEST URL ', url)
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+    // this.logger.log('JOIN FUNCT OPTIONS  ', options);
+    let body = {}
+    body = { 'sender': requester_id, 'subject': subject, 'text': message, 'departmentid': departmentid, 'channel': { 'name': 'form' }, 'priority': ticketpriority };
+    if (participantid !== undefined) {
+      body['participants'] = [participantid]
+    } else {
+      body['participants'] = participantid
+    }
+    // , 'participants': [participantid]
+
+
+    console.log('[WS-REQUESTS-SERV] - CREATE INTERNAL REQUEST body ', body);
+    return this.http
+    .post(url, body, httpOptions)
+    .pipe(map((res: any) => {
+      console.log('[TILEDESK-SERVICE] - CREATE NEW LEAD RES ', res);
+      return res
+    }))
+  }
   
 
 
